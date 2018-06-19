f00b109c29e773ad9a0512256038b74021b6fff0
johnnynode <johnnynode@gmail.com>
2018-06-19 20:37:23 +0800

    add demo code comments

--- Basic-Python/code/demo.py
+++ Basic-Python/code/demo.py
@@ -7,8 +7,8 @@ curved = uf.add_five(scores)
 
 mean_c = uf.mean(curved)
 
-print("Scores:", scores)
-print("Original Mean:", mean, " New Mean:", mean_c)
+print("Scores:", scores) # Scores: [88, 92, 79, 93, 85]
+print("Original Mean:", mean, " New Mean:", mean_c) # Original Mean: 87.4  New Mean: 92.4
 
-print(__name__)
-print(uf.__name__)
+print(__name__) # __main__
+print(uf.__name__) # useful_functions
