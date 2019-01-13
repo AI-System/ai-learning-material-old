96b0f46634bab23f36a5cd69de51655f840ffad3
johnnynode <johnnynode@gmail.com>
2019-01-13 15:33:31 +0800

    import keyword

---
+++ Basic-Python/code/import_keyword.py
@@ -0,0 +1,8 @@
+import keyword
+
+print(keyword.kwlist)
+
+'''
+标识符不能使用关键字！！！
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+'''
