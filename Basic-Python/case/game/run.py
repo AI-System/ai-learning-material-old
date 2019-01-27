29abd222bd7efc4031734a6ac32556c4edad26dc
johnnynode <johnnynode@gmail.com>
2019-01-27 22:06:58 +0800

    game plane, something wrong

--- Basic-Python/case/game/run.py
+++ Basic-Python/case/game/run.py
@@ -2,8 +2,28 @@ import pygame
 from pygame.locals import *
 import time
 
+# 创建飞机对象
+class Plane:
+  def __init__(self, screen):
+    self.x = 200
+    self.y = 400
+    self.screen = screen
+    self.image = pygame.image.load("./images/me.png")
+
+  def display(self):
+    ''' 绘制飞机功能 '''
+    self.screen.blit(self.image, (self.x, self.y))
+  
+  def move(self, flag):
+    ''' flag True 左, False 右'''
+    self.x -= 5
+    if flag:
+      self.x -= 5
+    else:
+      self.x += 5
+
 # 键盘控制函数
-def key_control():
+def key_control(plane):
     ''' 键盘控制函数 '''
     # 执行退出操作
     for event in pygame.event.get():
@@ -15,8 +35,10 @@ def key_control():
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[K_LEFT] or pressed_keys[K_a]:
       print('left ...')
+      plane.move(True)
     elif pressed_keys[K_RIGHT] or pressed_keys[K_d]:
       print('right ...')
+      plane.move(False)
 
     if pressed_keys[K_SPACE]:
       print('space ...')
@@ -28,6 +50,8 @@ def main():
   bg = pygame.image.load("./images/bg2.jpg")
   m = -968 # 图片高 1536 , 画布高 568 , 将图片置地, -968是初始化时候的坐标位置 即从零点向上移动968像素, 最终底部对齐的效果
   # 注：因为涉及到无缝滚动, 所以画布是两张图片拼起来的, 一张图片的高度是 1536 / 2 = 768
+  # 创建飞机对象
+  plane = Plane(screen)
   while True:
     # 绘制画面
     screen.blit(bg, (0, m))
@@ -35,7 +59,7 @@ def main():
     if m >= -200: # 当移动完一张图片的位置之后，那么重新绘制 -968 + 768 = -200
       m = -968
       # 键盘控制
-      key_control()
+      key_control(plane)
       # 更新显示
       pygame.display.update()
       # 定时显示
