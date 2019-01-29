6da66a1bd3c2eecb6cc3368629910560c1e02a23
johnnynode <johnnynode@gmail.com>
2019-01-29 10:08:49 +0800

    fix

--- Basic-Python/case/countFolderSize/library/count.py
+++ Basic-Python/case/countFolderSize/library/count.py
@@ -9,10 +9,12 @@ def count(total, path):
     file = os.path.join(path, f) # 拼合文件路径
     # 判断是否是文件
     if os.path.isfile(file):
+      print('is file: ！')
+      print(file)
       # 注：此处有两种获取文件大小的方式 os.path.getsize(file) 和 os.stat(file).st_size
       total += os.path.getsize(file)
     ## 判断是否是目录
     if os.path.isdir(file):
       print('file folder: ', file)
-      count(total, file) # 递归
+      total = count(total, file) # 递归
   return total
