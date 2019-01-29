62a52fa1cf5b58282741c53b2ae23e04d270affd
johnnynode <johnnynode@gmail.com>
2019-01-29 09:34:17 +0800

    finish count folder size function

---
+++ Basic-Python/case/countFolderSize/run.py
@@ -0,0 +1,5 @@
+from lib.count import count
+
+total = 0 # 初始化总大小
+total = count(total, './test')
+print('test目录的数据大小为：', str(total) + ' Bytes', ' 等于： ' + str(total / 1024), 'KB') # test目录的数据大小为： 6148 Bytes  等于： 6.00390625 KB
