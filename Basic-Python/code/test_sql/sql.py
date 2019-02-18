60e876aa6b61a1108705c174b65d70f8c17190a7
johnnynode <johnnynode@gmail.com>
2019-02-18 20:59:30 +0800

    [test] mysql

---
+++ Basic-Python/code/test_sql/sql.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+# 导入 pymysql 模块
+import pymysql
+
+# 获取数据库连接
+db = pymysql.connect(host="localhost", user="root", password="123456", db="mydb", charset="utf8")
+
+
+# 创建游标对象
+cursor = db.cursor()
+
+# 定义sql语句
+sql = 'select * from stu'
+
+# 执行sql语句
+try:
+    cursor.execute(sql)
+    print('result ', cursor.rowcount)
+except:
+    print('SQL执行错误!')
+
+# 关闭数据库连接
+db.close()
