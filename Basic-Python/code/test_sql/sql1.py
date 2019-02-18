593ce50761431da788c13563a91e006c59441491
johnnynode <johnnynode@gmail.com>
2019-02-18 21:08:40 +0800

    [test] sql1

--- Basic-Python/code/test_sql/sql1.py
+++ Basic-Python/code/test_sql/sql1.py
@@ -16,16 +16,12 @@ sql = 'select * from stu'
 # 执行sql语句
 try:
     cursor.execute(sql)
-    
-    print('result ', cursor.rowcount)
-    data1 = cursor.fetchone() # test 一条数据
-    print(data1)
-    
-    print('all data')
-    dataall = cursor.fetchall() # test 多条数据 两者同时使用会影响 fetchall 的结果
-    print(dataall)
+    while True:
+        data = cursor.fetchone()
+        if data == None:
+            break
+        print(data)
 except:
-
     print('SQL执行错误!')
 
 # 关闭数据库连接
